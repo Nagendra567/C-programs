f801da1e85bb6df8e95ba4226f382bf657dd7106
Nagendra567 <36262479+Nagendra567@users.noreply.github.com>
2019-07-16 19:45:22 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+	char n,a[5]={'a','e','i','o','u'};
+	int i,flag=0;
+	scanf("%c",&n);
+	if(n='a'&&n<='z')
+	{
+		for(i=0;i<=5;i++)
+		{
+			if(n==a[i])
+			{
+				flag=1;
+				break;
+			}
+			}
+			if(flag==1)
+			{
+				printf("Vowel");
+				
+			}
+			else
+			{
+			printf("consonant");
+		}
+	}
+		else
+		{
+		
+		printf("Invaid");
+	}
+		return 0;
+			
+		}
+		
+	
+	
